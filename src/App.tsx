fd44c5d3323c2fc2ee3748e03faf7574e73c8611
portora1 <nakajinn.1up@gmail.com>
2025-07-25 17:59:29 +0900

    入力フォーム実装・Submitのeを削除

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 type Todo = {
   value: string;
+  readonly id: number;
 };
 
 export const App = () => {
@@ -13,11 +14,29 @@ export const App = () => {
   };
   const handleSubmit = () => {
     if (!text) return; 
-    const newTodo: Todo = { value: text };
+    const newTodo: Todo = {
+      value: text,
+      id: new Date().getTime(), 
+    };
   
-  setTodos([...todos, newTodo]);
+  setTodos((todos) => [newTodo, ...todos]);
   setText('');
   };
+  const handleEdit = (id: number, value: string) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo,value: value};
+        }
+        return todo;
+      });
+      /** console.log('=== originaru todos ===');
+      todos.map((todo) => {
+        console.log(`id: ${todo.id}, value: ${todo.value}`);
+      })*/
+      return newTodos;
+  });
+};
 
   return (
     <div>
@@ -29,6 +48,19 @@ export const App = () => {
         <input type="text" value={text} onChange={(e) => handleChange(e)} />
         <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
+      <ul>
+        {todos.map((todo) => {
+          return (
+          <li key={todo.id}>
+            <input 
+              type="text"
+              value={todo.value}
+              onChange={(e) => handleEdit(todo.id, e.target.value)}
+              />
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
