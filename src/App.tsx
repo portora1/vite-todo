2e06dff2322ef64f344f3213278bf857aa45c776
portora1 <nakajinn.1up@gmail.com>
2025-07-25 23:34:20 +0900

    削除ボタンと機能追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ type Todo = {
   value: string;
   readonly id: number;
   checked: boolean;
+  removed: boolean
 };
 
 export const App = () => {
@@ -18,6 +19,7 @@ export const App = () => {
       value: text,
       id: new Date().getTime(), 
       checked: false,
+      removed: false
     };
   
     setTodos((todos) => [newTodo, ...todos]);
@@ -49,6 +51,17 @@ export const App = () => {
       return newTodos;
     });
   };
+  const handoleRemove = (id: number, removed: boolean) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, removed };
+        }
+        return todo;
+      });
+      return newTodos;
+    });
+  };
 
   return (
     <div>
@@ -66,15 +79,17 @@ export const App = () => {
           <li key={todo.id}>
             <input
               type="checkbox"
+              disabled={todo.removed}
               checked={todo.checked}
               onChange={() => handleCheck(todo.id, !todo.checked)}
               />
             <input
               type="text"
-              disabled={todo.checked}
+              disabled={todo.checked || todo.removed}
               value={todo.value}
               onChange={(e) => handleEdit(todo.id, e.target.value)}
               />
+            <button onClick={() => handoleRemove(todo.id, !todo.removed)}>{todo.removed ? '復元' : '削除'}</button>
             </li>
           );
         })}
