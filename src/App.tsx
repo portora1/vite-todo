a06ecdb69cbec0e32771523059e7bede6c6fc7aa
portora1 <nakajinn.1up@gmail.com>
2025-07-27 11:38:21 +0900

    handleTodoで３つの関数をまとめる

--- src/App.tsx
+++ src/App.tsx
@@ -21,52 +21,37 @@ export const App = () => {
       value: text,
       id: new Date().getTime(), 
       checked: false,
-      removed: false
+      removed: false,
     };
   
     setTodos((todos) => [newTodo, ...todos]);
     setText('');
   };
-  const handleEdit = (id: number, value: string) => {
+
+    const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
+      id: number,
+      key: K, 
+      value: V
+    ) => {
     setTodos((todos) => {
       const newTodos = todos.map((todo) => {
         if (todo.id === id) {
-          return { ...todo,value: value};
+          return { ...todo, [key]: value };
+        } else {
+          return todo;
         }
-        return todo;
-      });
-      /** console.log('=== originaru todos ===');
-      todos.map((todo) => {
-        console.log(`id: ${todo.id}, value: ${todo.value}`);
-      })*/
-      return newTodos;
-  });
-};
-    const handleCheck = (id: number, checked: boolean) => {
-    setTodos((todos) => {
-      const newTodos = todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, checked };
-        }
-        return todo;
-      });
-      return newTodos;
     });
-  };
-  const handoleRemove = (id: number, removed: boolean) => {
-    setTodos((todos) => {
-      const newTodos = todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, removed };
-        }
-        return todo;
-      });
-      return newTodos;
+
+    return newTodos;
     });
   };
   const handleFilter = (filter: Filter) => {
     setFilter(filter);
   };
+    const handleEmpty = () => {
+    setTodos((todos) => todos.filter((todo) => !todo.removed));
+  };
+
   const filteredTodos = todos.filter((todo) => {
     switch (filter) {
       case 'all':
@@ -81,10 +66,8 @@ export const App = () => {
         return todo;
     }
   });
-  const handleEmpty = () => {
-    setTodos((todos) => todos.filter((todo) => !todo.removed));
-  };
 
+  
   return (
     <div>
       <select defaultValue="all" onChange={(e) => handleFilter(e.target.value as Filter)}>
@@ -116,15 +99,15 @@ export const App = () => {
               type="checkbox"
               disabled={todo.removed}
               checked={todo.checked}
-              onChange={() => handleCheck(todo.id, !todo.checked)}
+              onChange={() => handleTodo(todo.id, 'checked', !todo.checked)}
               />
             <input
               type="text"
               disabled={todo.checked || todo.removed}
               value={todo.value}
-              onChange={(e) => handleEdit(todo.id, e.target.value)}
+              onChange={(e) => handleTodo(todo.id, 'value', e.target.value)}
               />
-            <button onClick={() => handoleRemove(todo.id, !todo.removed)}>{todo.removed ? '復元' : '削除'}</button>
+            <button onClick={() => handleTodo(todo.id, 'removed', !todo.removed)}>{todo.removed ? '復元' : '削除'}</button>
             </li>
           );
         })}
